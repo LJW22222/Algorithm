e14e71bb0ce8291b0a562944dc19cdc826cbacbd
JongWon Lee <106072775+LJW22222@users.noreply.github.com>
2025-09-11 15:23:34 +0900

    [Bronze I] Title: 2진수 8진수, Time: 2152 ms, Memory: 99776 KB -BaekjoonHub

---
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(sc.nextLine());
+        String a = st.nextToken();
+        String string = new BigInteger(a, 2).toString(8);
+        System.out.println(string);
+    }
+}
